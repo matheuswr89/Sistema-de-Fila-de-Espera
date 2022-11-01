9cc40d32da2aeb7a2ff8564402f23ff21342bd2d
Matheus William <matheuswr89@outlook.com>
2022-10-31 21:49:45 -0300

    Subindo começo parte da WEB

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,21 @@
-import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { firebaseAuth } from "./database/firebaseConfig";
 import Home from "./pages/Home";
 
 function App() {
   let authenticated = false;
   let navigate = useNavigate();
   useEffect(() => {
-    onAuthStateChanged(firebaseAuth, (user) => {
-      if (user) {
-        const uid = user.uid;
-        authenticated = true;
-        navigate("/");
-      } else {
-        navigate("/login");
-        authenticated = false;
-      }
-    });
+    // onAuthStateChanged(firebaseAuth, (user) => {
+    //   if (user) {
+    //     const uid = user.uid;
+    //     authenticated = true;
+    //     navigate("/");
+    //   } else {
+    //     navigate("/login");
+    //     authenticated = false;
+    //   }
+    // });
   }, []);
 
   return (
